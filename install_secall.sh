832fd3a7cc4a2fec78ed6a27ceb3b3a1214abd67
Hibuz <38822671+hibuz@users.noreply.github.com>
2026-05-24 00:52:15 +0000

    bump secall v0.6.0

--- install_secall.sh
+++ install_secall.sh
@@ -2,8 +2,15 @@ npm i -g pnpm
 
 git clone https://github.com/hang-in/seCall.git && cd seCall
 
-cd web && pnpm install && pnpm build && cd ..
+cd web && pnpm install && pnpm approve-builds && pnpm build && cd ..
 
 cargo build --release
+# v0.6.0
+cp target/release/secall ../binary/
 
-sudo cp target/release/secall /usr/local/bin/
+wget -O kiwi.tgz https://github.com/bab2min/Kiwi/releases/download/v0.23.1/kiwi_lnx_x86_64_v0.23.1.tgz 
+tar zxf kiwi.tgz
+cp lib/libkiwi.so ../binary/
+
+#sudo cp binary/secall /usr/local/bin/
+#sudo cp binary/libkiwi.so /usr/local/lib/
